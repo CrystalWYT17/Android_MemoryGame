2149d45578500479fa8aea3d2fd9f05db0417a51
CrystalWYT17 <wuttyeetun1721999@gmail.com>
2023-08-26 16:42:03 +0800

    initial commit

---
+++ app/src/main/java/com/example/MatchingGame/LaunchActivity.java
@@ -0,0 +1,68 @@
+package com.example.MatchingGame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    private Button startButton;
+
+    private Intent BgmSoundIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        startButton = findViewById(R.id.startButton);
+        // start BGMService
+        playBackgroundSound();
+        Toast.makeText(this,"Now playing BGM",Toast.LENGTH_SHORT).show();
+
+        // To launch the Main Activity
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LaunchActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void playBackgroundSound(){
+        BgmSoundIntent = new Intent(this, AppBGMService.class);
+        startService(BgmSoundIntent);
+    }
+    public void stopBackgroundSound(){
+        if(BgmSoundIntent !=null){
+            stopService(BgmSoundIntent);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopBackgroundSound();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopBackgroundSound();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        playBackgroundSound();
+    }
+}
---
+++ app/src/main/java/com/example/MatchingGame/MyApplication.java
@@ -0,0 +1,18 @@
+package com.example.MatchingGame;
+
+import android.app.Application;
+import android.graphics.Bitmap;
+
+import java.util.List;
+
+public class MyApplication extends Application {
+    private List<Bitmap> selectedImages;
+
+    public List<Bitmap> getSelectedImages() {
+        return selectedImages;
+    }
+
+    public void setSelectedImages(List<Bitmap> selectedImages) {
+        this.selectedImages = selectedImages;
+    }
+}
